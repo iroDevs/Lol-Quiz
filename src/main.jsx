4e85350b90a417fce977268bbcaf610069686138
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2023-03-26 21:05:30 -0300

    React Router + fakeApi Return

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/Home/Home'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Home />,
+  },
+  ]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
---
+++ src/pages/Quiz/Quiz.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './HomeStyle.css';
+
+function Quiz() {
+  return (
+    <h1>Pagina Home</h1>
+  )
+}
+
+export default Quiz;
